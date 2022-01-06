93beba6fcafb2e0c66931e111683869fd74eedf1
Nikhil Srivastava <nikhilsrivastava@Nikhils-MacBook-Air.local>
2022-01-06 17:40:40 +0530

    taking user input

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 let inputArr = process.argv.slice(2);
-console.log(inputArr);
+console.log(inputArr);          //user will enter the command
+
+let command = inputArr[0];
+switch(command){
+    case "tree":
+        break;
+    case "organize":
+        break;
+    case "help":
+        break;
+    default:
+        console.log("Please enter a valid command");
+}
